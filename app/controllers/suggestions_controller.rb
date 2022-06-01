0abc6adcf2ed1a8ac9763e70533a192a88225237
Robert Gautsch <robert.gautsch93@gmail.com>
2022-06-01 17:07:32 +0200

    devise corrections

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,6 +1,10 @@
 class SuggestionsController < ApplicationController
   def index
-
-    @suggestions = Suggestion.all
+    @user = current_user
+    @assessments = @user.assessments
+    @answers = @assessments.answers
+    @suggestions = @answers.suggestions
+    # @suggestions = Suggestion.where(answer.assessment.user_id: current_user.id)
+    puts "#{@suggestions}"
   end
 end
