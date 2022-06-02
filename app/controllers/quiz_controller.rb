3e541aa1ac8705fee2189a0ebffa255b1711c845
Robert Gautsch <robert.gautsch93@gmail.com>
2022-06-02 11:48:17 +0200

    suggestion logic

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,4 +1,11 @@
 class QuizController < ApplicationController
   def start
   end
+
+  def result
+    @goods = current_user.answers.where(answer_type: "good")
+    @bads = current_user.answers.where(answer_type: "bad")
+    @neutrals = current_user.answers.where(answer_type: "neutral")
+    # User.first.answers.where(answer_type: "bad").last.question.category
+  end
 end
