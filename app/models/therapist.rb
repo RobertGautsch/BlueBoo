8865f8d5ffaf504bd505d5d7f2e3facb6cdd0442
Robert Gautsch <robert.gautsch93@gmail.com>
2022-06-06 16:40:21 +0200

    Implemented basic map in therapist index

--- app/models/therapist.rb
+++ app/models/therapist.rb
@@ -1,8 +1,6 @@
 class Therapist < ApplicationRecord
   has_many :therapist_therapy_types, dependent: :destroy
   has_many :therapy_types, through: :therapist_therapy_types, dependent: :destroy
-
-  def index
-    @therapists = Therapist.all
-  end
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
